01626d672cc873597b27ce95445e67dec859550c
Valentin <107222626+mokarey@users.noreply.github.com>
2023-12-06 20:10:02 -0300

    VIEWS WORKING

--- triggers_sql.sql
+++ triggers_sql.sql
@@ -91,8 +91,11 @@ SELECT contarEquiposDiamante() AS CantidadEquiposDiamante;
 
 -- TRIGER NUMERO 1
 -- TABLA LOG
+SELECT CURRENT_USER();
 
 USE torneo;
+DESCRIBE equipos_categoria_log;
+
 CREATE TABLE equipos_categoria_log (
     id_equipos_categoria_log INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     id_equipos INT NOT NULL,
@@ -103,8 +106,11 @@ CREATE TABLE equipos_categoria_log (
     fecha_log TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-
 -- TRIGGER
+
+USE torneo;
+INSERT INTO equipos_categoria_log (id_equipos, nombre, niveles, posiciones)  
+VALUES ('2', 'Equipo 2', 'Diamante', 2);
 
 USE torneo;
 SELECT * FROM equipos_categoria_log;
@@ -116,10 +122,61 @@ CREATE TRIGGER after_insert_equipos_categoria
 AFTER INSERT ON equipos_categoria
 FOR EACH ROW
 BEGIN
-    INSERT INTO equipos_categoria_log (id_equipos, nombre, niveles, posiciones, usuario, fecha_log)
+    INSERT INTO equipos_categoria_log (id_equipos, nombre, niveles, posiciones, usuario_log, fecha_log)
     VALUES (NEW.id_equipos, NEW.nombre, NEW.niveles, NEW.posiciones, CURRENT_USER(), CURRENT_TIMESTAMP);
 END //
 
 DELIMITER ;
 
+-- TRIGGER NUMERO 2
+-- TABLA LOG 2
 
+USE torneo;
+CREATE TABLE jugadores_log (
+    id_jugadores_log INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    id_jugadores INT NOT NULL,
+    nombre VARCHAR(30) NOT NULL,
+    nivel INT NOT NULL,
+    equipamiento VARCHAR(150) NOT NULL,
+    usuario_log VARCHAR(50),
+    fecha_log TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+USE torneo;
+select * from jugadores_log;
+
+INSERT INTO jugadores_log (id_jugadores, nombre, nivel, equipamiento)  
+VALUES ('2', 'Juan', 14, 'Hacha');
+
+-- TRIGGER 2
+USE torneo;
+DELIMITER $$
+
+CREATE TRIGGER after_insert_jugadores
+AFTER INSERT ON jugadores
+FOR EACH ROW
+BEGIN
+    INSERT INTO jugadores_log (id_jugadores, nombre, nivel, equipamiento, usuario_log, fecha_log)
+    VALUES (NEW.id_jugadores, NEW.nombre, NEW.nivel, NEW.equipamiento, CURRENT_USER(), CURRENT_TIMESTAMP);
+END $$
+
+DELIMITER ;
+
+-- VISTAS 
+-- VISTA 1
+
+CREATE VIEW niveles_mayores_a_5 AS
+SELECT nombre, nivel FROM jugadores WHERE nivel > 5;
+
+USE torneo;
+SELECT * FROM niveles_mayores_a_5;
+
+-- VISTA 2
+
+CREATE VIEW equipos_diamante AS
+SELECT nombre FROM torneo.equipo_categoria WHERE niveles = 'Diamante';
+    
+USE torneo;
+SELECT * FROM equipos_diamante;
+
+--
